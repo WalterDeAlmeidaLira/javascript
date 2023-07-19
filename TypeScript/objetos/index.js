4714dcdb51fcb339219f93947393f1800411342a
walterAL <walteralmeidalira@gmail.com>
2023-07-18 21:19:40 -0300

    estudos de objetos com typescript

--- TypeScript/objetos/index.js
+++ TypeScript/objetos/index.js
@@ -14,3 +14,43 @@ function criaPersonagem(obj) {
 }
 ;
 criaPersonagem(jogadorUm);
+;
+var u1 = {
+    email: "teste@teste.com"
+};
+var u2 = {
+    email: "teste1@teste1.com",
+    role: "profissaoDeTeste"
+};
+function showUser(user) {
+    console.log("usu\u00E1rio: ".concat(user.email));
+    if (user.role) {
+        console.log("sua fun\u00E7\u00E3o \u00E9 ".concat(user.role));
+    }
+    ;
+}
+;
+showUser(u1);
+showUser(u2);
+var palio = {
+    marca: 'Fiat',
+    rodas: '4'
+};
+var coordenadas = {
+    x: 10
+};
+coordenadas.y = 15;
+console.log(coordenadas);
+var olavo = {
+    nome: 'Olavo',
+    idade: 40,
+    salario: 7000
+};
+console.log(olavo);
+var teste = [1, 2, 3, 4];
+// tuplas readonly
+function exibirNumeros(numeros) {
+    console.log(numeros[0]);
+    console.log(numeros[1]);
+}
+exibirNumeros([10, 20]);
--- TypeScript/objetos/index.ts
+++ TypeScript/objetos/index.ts
@@ -23,3 +23,90 @@ function criaPersonagem(obj: Jogador){
 };
 
 criaPersonagem(jogadorUm);
+
+//INTERFACE COM PROPRIEDADE OPCIONAL
+
+interface User{
+    email: string
+    role?: string
+};
+
+const u1: User = {
+    email: "teste@teste.com"
+};
+
+const u2: User = {
+    email: "teste1@teste1.com",
+    role:"profissaoDeTeste"
+};
+
+function showUser(user: User){
+    console.log(`usuário: ${user.email}`);
+    if(user.role){
+        console.log(`sua função é ${user.role}`);
+    };
+};
+
+showUser(u1);
+showUser(u2);
+
+// readonly 
+
+interface Carro{
+    marca: string
+    readonly rodas: string
+}
+
+const palio: Carro = {
+    marca: 'Fiat',
+    rodas: '4'
+}
+
+// index signature
+
+interface Coordenadas{
+    [index: string]: number
+}
+
+const coordenadas: Coordenadas = {
+    x: 10
+}
+
+coordenadas.y = 15
+
+console.log(coordenadas);
+
+// UNIÃO DE TIPOS 
+interface Usuario{
+    nome: string
+    idade: number
+}
+
+interface Funcionario{
+    salario: number
+}
+
+type Engenheiro = Usuario & Funcionario;
+
+const olavo: Engenheiro = {
+    nome: 'Olavo',
+    idade: 40,
+    salario: 7000
+}
+
+console.log(olavo);
+
+// tupla 
+
+type array = [number,number,number,number];
+
+const teste: array = [1,2,3,4];
+
+// tuplas readonly
+function exibirNumeros(numeros: readonly[number,number]){
+    console.log(numeros[0])
+    console.log(numeros[1])
+}
+
+
+exibirNumeros([10,20]);
